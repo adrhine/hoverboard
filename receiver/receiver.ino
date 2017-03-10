4142ee2a4e122a499e7a6a8f0b365217e608bcf9
Andrew Rhine <Andrew Rhine>
2017-03-10 00:16:15 -0500

    cleaned + updated receiver. works off the new controller struct

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -3,35 +3,28 @@
 #include <RF24_config.h>
 
 #include <SPI.h>
-int msg[2];
+#include ".\controller.cpp"
+
 RF24 radio(9,10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
-int LED1 = 2;
+//int LED1 = 2;
+controller cntrl = {0,0};
 
 void setup(void){
  Serial.begin(9600);
  radio.begin();
  radio.openReadingPipe(1,pipe);
  radio.startListening();
- pinMode(LED1, OUTPUT);
+ //pinMode(LED1, OUTPUT);
 }
 void loop(void){
  if (radio.available()){
-   bool done = false;    
-   while (!done){
-     done = radio.read(msg, 10);      
-     Serial.println(msg[0]);
-     if (msg[0] == 2){
-       delay(1000);
-       //Serial.println(msg[1]);
-       Serial.println(msg[2]);
-       digitalWrite(LED1, HIGH);
-     }
-     else {
-       digitalWrite(LED1, LOW);
-     }
-     delay(1);
+     radio.read(&cntrl, sizeof(cntrl));
+     Serial.println(cntrl.potMtr);
+     Serial.println(cntrl.thmbStick);
+     delay(100);
    }
+ else{
+  Serial.println("No radio available");
  }
- else{Serial.println("No radio available");}
 }
