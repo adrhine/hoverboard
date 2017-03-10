3c6992f2616d54812e39014d7b1772cdbd252b4c
AndrewRhine <rhine.andrew@gmail.com>
2017-03-09 23:03:14 -0500

    test2

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,17 +1,24 @@
 #include  <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
-int msg[1];
+int msg[2];
 RF24 radio(9,10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 void setup(void){
  Serial.begin(9600);
  radio.begin();
  radio.openWritingPipe(pipe);
+ pinMode(0, INPUT);
 }
 void loop(void){
-  int val = analogRead(2);
-  Serial.println(val);
-  msg[0] = val;
-  radio.write(msg, 2);
+  int numOfInputs = 2;
+  int thmbStick = analogRead(0);
+  int potMtr = analogRead(1);
+  //Serial.println(val);
+  msg[0] = numOfInputs;
+  //msg[1] = thmbStick;
+  msg[1] = potMtr;
+  Serial.println(thmbStick);
+    Serial.println(potMtr);
+  radio.write(msg, 10); // 2 = #/bytes
 }
